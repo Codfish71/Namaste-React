5599666fe78998e6752ba06058fabe13f02750cc
Codfish71 <prathameshpendal71@gmail.com>
2023-08-07 23:42:48 +0530

    Assignment for Leacture 3

--- Foundation of React/App.js
+++ Foundation of React/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -8,12 +9,18 @@ const title = <h1>Title of the page</h1>;
 const Heading = () => {
   return (
     <div className="container">
-      {title}
-      <h1>React component</h1>;
+      <h2 className="brand">Brand</h2>
+      <input
+        type="text"
+        placeholder="Enter any keyword"
+        className="searchbar"
+      />
+      <img
+        src="https://w7.pngwing.com/pngs/81/570/png-transparent-profile-logo-computer-icons-user-user-blue-heroes-logo-thumbnail.png"
+        className="profilePic"
+      />
     </div>
   );
 };
-
-const Heading2 = () => <h1>This is also a react functional component</h1>;
 
 root.render(<Heading />);
