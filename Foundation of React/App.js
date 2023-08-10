4e260c9a3038fcc82be40fea23b0496a6ca293b2
Codfish71 <prathameshpendal71@gmail.com>
2023-08-11 01:39:18 +0530

    Food website completed[C

--- Foundation of React/App.js
+++ Foundation of React/App.js
@@ -9,16 +9,7 @@ const title = <h1>Title of the page</h1>;
 const Heading = () => {
   return (
     <div className="container">
-      <h2 className="brand">Brand</h2>
-      <input
-        type="text"
-        placeholder="Enter any keyword"
-        className="searchbar"
-      />
-      <img
-        src="https://w7.pngwing.com/pngs/81/570/png-transparent-profile-logo-computer-icons-user-user-blue-heroes-logo-thumbnail.png"
-        className="profilePic"
-      />
+      
     </div>
   );
 };
